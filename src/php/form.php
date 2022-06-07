3be858e8e562199358a130311d633c0035c4b63f
lepremieraxel <axelmarcial151003@gmail.com>
2022-06-07 11:14:34 +0200

    maj

--- src/php/form.php
+++ src/php/form.php
@@ -1,17 +1,19 @@
 <?php
 
-if(isset($_POST['name']) && isset($_POST['firstname']) && isset($_POST['email']) && isset($_POST['message']) && !empty($_POST['name']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])){
+header('Location: ../../');
+
+if (isset($_POST['name']) && isset($_POST['firstname']) && isset($_POST['email']) && isset($_POST['message'])) {
 
   $name = $_POST['name'];
   $firstname = $_POST['firstname'];
   $email = $_POST['email'];
   $message = $_POST['message'];
 
-  $to = 'hello@axelmarcial.com';
+  $to = 'axelmarcial151003@gmail.com';
   $object = '[SITE] - Nouveau message sur la page d\'attente';
-  $content = `Nouveau message de $firstname $name.\n\n Message : $message`;
-  $headers = `From: hello@axelmarcial.com\r\nReply-To: $email`;
+  $content = 'Nouveau message de ' . $firstname . $name. '\r Message : ' . $message;
 
-  mail($to, $object, $content, $headers);
-  header('Location:../../'); die();
-} else header('Location:../../'); die();
+  mail($to, $object, $content);
+} else {
+  header('Location: ../../');
+}
